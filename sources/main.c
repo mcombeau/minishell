12c0b1ea0e3edfb3f778f6e7ee911a9c5359436e
Alexandra Quesada <alexandra.quesad@gmail.com>
2022-07-19 20:11:44 +0200

    fist test pushing

--- sources/main.c
+++ sources/main.c
@@ -4,7 +4,16 @@ int	main(int ac, char **av, char **env)
 {
 	(void)ac;
 	(void)av;
+	char *str;
+
 	test_env(ac, av, env);
+	while (1)
+	{
+		str = readline("\033[0;35m➜\033[0;m \033[0;33mMinishell >\033[0;m");
+		printf("str = %s\n", str);
+		add_history(str);
+		free(str);
+	}
 	exit_shell();
 	return (0);
 }
