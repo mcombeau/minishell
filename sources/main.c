6ca4af83f1d9e0c65d00d8f571d168b1651177a7
Mia <miacombeau@protonmail.com>
2022-07-19 23:01:53 +0200

    removed test from main, init env in main

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,9 @@ int	main(int ac, char **av, char **env)
 	(void)av;
 	char *str;
 
-	test_env(ac, av, env);
+	if (!init_env(env))
+		// print error message here.
+		exit_shell(); // exit with error code.
 	while (1)
 	{
 		str = readline("\033[0;35m➜\033[0;m \033[0;33mMinishell >\033[0;m");
