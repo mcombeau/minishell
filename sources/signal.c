9762b40149d425650f7422389dc71d15895a9bc0
Mia <miacombeau@protonmail.com>
2022-07-20 13:21:13 +0200

    fixed SIGQUIT signal handling

--- sources/signal.c
+++ sources/signal.c
@@ -7,11 +7,13 @@
 */
 void	handle_signal(int signo)
 {
-	if (signo == SIGINT || signo == SIGQUIT)
+	if (signo == SIGINT)
 	{
 		ft_putchar_fd('\n', STDOUT_FILENO);
 		ft_putstr_fd(PROMPT, STDOUT_FILENO);
 		signal(SIGINT, handle_signal);
+	}
+	if (signo == SIGQUIT){
 		signal(SIGQUIT, handle_signal);
 	}
 }
