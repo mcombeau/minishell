8aeb948df9a8862800d9fe34a68914cebb3ed6ba
Mia <miacombeau@protonmail.com>
2022-09-20 16:54:13 +0200

    fix heredoc interpreted only on first command

--- sources/parser/parse_heredoc.c
+++ sources/parser/parse_heredoc.c
@@ -1,17 +1,16 @@
 #include "minishell.h"
 
 //TODO: check if several heredocs can be opened at the same time.
-//		heredoc might also be able to be opened after a pipe. Check for that too.
 void	parse_heredoc(t_data *data, t_command **last_cmd, t_token **token_lst)
 {
 	t_token		*temp;
-	t_command	*first_cmd;
+	t_command	*cmd;
 	t_io_fds	*io;
 
 	temp = *token_lst;
-	first_cmd = *last_cmd;
-	init_io(first_cmd);
-	io = first_cmd->io_fds;
+	cmd = lst_last_cmd(*last_cmd);
+	init_io(cmd);
+	io = cmd->io_fds;
 	io->infile = ft_strdup("/tmp/.minishell_heredoc");
 	io->heredoc_delimiter = ft_strdup(temp->next->str);
 	if (get_heredoc(data, io))
