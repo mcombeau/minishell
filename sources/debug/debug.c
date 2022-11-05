99640fa0fcde2b748b97713ec1497ae35ffcbb20
Mia <miacombeau@protonmail.com>
2022-11-05 12:28:37 +0100

    norminette debug

--- sources/debug/debug.c
+++ sources/debug/debug.c
@@ -1,35 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   debug.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcombeau <mcombeau@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/05 12:17:42 by mcombeau          #+#    #+#             */
+/*   Updated: 2022/11/05 12:28:16 by mcombeau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
+
+static void	print_cmd_args(t_command *cmd)
+{
+	int	i;
+
+	if (!cmd->args)
+		return ;
+	i = 0;
+	while (cmd->args[i])
+	{
+		printf("\tArgs[%d] = %s\n", i, cmd->args[i]);
+		i++;
+	}
+}
+
+static void	print_cmd_io(t_command *cmd)
+{
+	if (!cmd->io_fds)
+		return ;
+	if (cmd->io_fds->infile)
+	{
+		printf("\tInfile: %s\n", cmd->io_fds->infile);
+		printf("\t\tfd_in: %d\n", cmd->io_fds->fd_in);
+	}
+	if (cmd->io_fds->heredoc_delimiter)
+		printf("\tHeredoc delimiter: %s\n", cmd->io_fds->heredoc_delimiter);
+	if (cmd->io_fds->outfile)
+	{
+		printf("\tOutfile: %s\n", cmd->io_fds->outfile);
+		printf("\t\tfd_in: %d\n", cmd->io_fds->fd_out);
+	}
+}
 
 void	print_cmd_list(t_data *data)
 {
-	t_command *cmd;
+	t_command	*cmd;
 
 	cmd = data->cmd;
 	printf("\n---- COMMAND LIST\n");
 	while (cmd)
 	{
 		printf("--- Command = %s\n", cmd->command);
-		if (cmd->args)
-		{
-			for (int i = 0; cmd->args[i]; i++)
-			printf("\tArgs[%d] = %s\n", i, cmd->args[i]);
-		}
+		print_cmd_args(cmd);
 		printf("\tPipe_output = %d\n", cmd->pipe_output);
-		if (cmd->io_fds)
-		{
-			if (cmd->io_fds->infile)
-			{
-				printf("\tInfile: %s\n", cmd->io_fds->infile);
-				printf("\t\tfd_in: %d\n", cmd->io_fds->fd_in);
-			}
-			if (cmd->io_fds->heredoc_delimiter)
-				printf("\tHeredoc delimiter: %s\n", cmd->io_fds->heredoc_delimiter);
-			if (cmd->io_fds->outfile)
-			{
-				printf("\tOutfile: %s\n", cmd->io_fds->outfile);
-				printf("\t\tfd_in: %d\n", cmd->io_fds->fd_out);
-			}
-		}
+		print_cmd_io(cmd);
 		if (cmd->prev == NULL)
 			printf("\tprev = NULL\n");
 		else
@@ -42,4 +68,24 @@ void	print_cmd_list(t_data *data)
 		cmd = cmd->next;
 	}
 	printf("\n");
+}
+
+void	print_token_list(t_data *data)
+{
+	t_token	*lst;
+	int		i;
+
+	lst = data->token;
+	printf("\n---- TOKEN LIST\n");
+	i = 0;
+	while (lst)
+	{
+		printf("--- Token [%d]\n", i);
+		printf("\tString = [%s]\n", lst->str);
+		printf("\tStr backup = [%s]\n", lst->str_backup);
+		printf("\tType = %d\n", lst->type);
+		printf("\tStatus = %d\n", lst->status);
+		i++;
+		lst = lst->next;
+	}
 }
